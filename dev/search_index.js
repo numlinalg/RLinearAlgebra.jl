f513092ca2be6cf494d3666eb9d159603ae7d5ac
Documenter.jl <documenter@juliadocs.github.io>
2021-05-19 20:09:39 +0000

    build based on 354dcef

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#RLinearAlgebra.jl","page":"RLinearAlgebra.jl","title":"RLinearAlgebra.jl","text":"","category":"section"},{"location":"","page":"RLinearAlgebra.jl","title":"RLinearAlgebra.jl","text":"Documentation for RLinearAlgebra.jl","category":"page"}]
+}
