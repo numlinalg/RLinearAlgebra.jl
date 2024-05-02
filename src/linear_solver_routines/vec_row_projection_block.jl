74b58c0912b7e2d1d18a083fe97dd96002683ee1
Nathaniel pritchard <npritchard@wisc.edu>
2024-05-02 09:55:08 -0500

    Added block row solver and updated git ignore

---
+++ src/linear_solver_routines/vec_row_projection_block.jl
@@ -0,0 +1,50 @@
+
+# This file is pat of RLinearAlgebra.jl
+# 1. Specifies type
+# 2. Implements rsubsolve function
+# 3. Exports Type
+
+#using LinearAlgebra
+
+"""
+    LinSysVecRowBlockProj <: LinSysVecRowProjection
+
+A mutable structure that represents a standard block row projection method.
+
+# Aliases
+- `BlockKaczmarz`
+
+# Fields
+- `alpha::Float64`, a relaxation parameter that should be set between `0.0` and `2.0`
+- `update::Union{Nothing, AbstractArray}` - A buffer for storing update.
+Calling `LinSysVecColBlockProj()` defaults the relaxatoin parameter to `1.0`.
+
+"""
+mutable struct LinSysVecRowBlockProj <: LinSysVecRowProjection
+    α::Float64
+    update::Union{Nothing, AbstractArray}
+end
+LinSysVecRowBlockProj(α) = LinSysVecRowBlockProj(α, nothing)
+
+LinSysVecRowBlockProj() = LinSysVecRowBlockProj(1.0, nothing)
+
+BlockKaczmarz = LinSysVecRowBlockProj
+# Common rsubsolve interface for linear systems
+function rsubsolve!(
+    type::LinSysVecRowBlockProj,
+    x::AbstractVector,
+    samp::Tuple{U,V,W} where {U<:Union{AbstractVector,AbstractMatrix},V<:AbstractArray,W<:AbstractVector},
+    iter::Int64,
+)
+    # samp[1] is the search direction
+    # samp[2] is the sketched matrix A
+    # samp[3] is the residual of system in A * samp[1], (samp[1] * A * x - b)
+    if iter == 1
+        p,n = size(samp[2])
+        type.update = Array{typeof(samp[2][1])}(undef, n)
+    end
+    fill!(type.update, 0)
+    LinearAlgebra.ldiv!(type.update, lq(samp[2]), samp[3])
+    x .-= type.update
+    return nothing
+end
