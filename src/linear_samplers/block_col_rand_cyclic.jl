ac96719b141bae19e0d680bed89fad6d0c19f432
Nathaniel pritchard <npritchard@wisc.edu>
2024-06-24 21:27:43 -0500

    remooved views that still remained after looping

--- src/linear_samplers/block_col_rand_cyclic.jl
+++ src/linear_samplers/block_col_rand_cyclic.jl
@@ -139,7 +139,6 @@ function LinearAlgebra.mul!(y::AbstractVector, Sampler::LinSysBlkColRandCyclic, 
     m,s = size(Sampler.AS)
     @assert s == size(x,1) "Right-hand array, x, must match sampling dimension"
     @assert m == size(y,1) "Left-hand array, y, row dimension of block"
-    @views axpby!(α, x, β, y[Sampler.S])
     for i in 1:s
         temp = y[Sampler.S[i]] * β
         y[Sampler.S[i]] += x[i] * α
