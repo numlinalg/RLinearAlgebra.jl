d377a76bc41a5422c2a6f3e148f54a381bce38dd
Nathaniel pritchard <npritchard@wisc.edu>
2024-07-11 09:58:55 -0500

    Added test for Gentlemans

---
+++ test/linear_solver_routines/block_solver_helpers/proc_gentlemans_house.jl
@@ -0,0 +1,49 @@
+# This file is part of RLinearAlgebra.jl
+
+module ProceduralTestGent
+
+using Test, RLinearAlgebra, LinearAlgebra, Random
+
+@testset "Gent Projection -- Procedural" begin
+    A = rand(10,5)
+    x = rand(5)
+    b = A * x
+    G = RLinearAlgebra.GentData(A, 3)
+    # Check sizes of block matrix B 
+    m, n = size(G.B)
+    @test m == 3 + 1 + 5
+    @test n == 5 + 1
+
+    #Test block copy function 
+    RLinearAlgebra.copy_block_from_mat!(G.B, A, b, 1:3)
+    @test A[1:3, :] == G.B[7:9, 1:5] 
+    @test b[1:3] == G.B[7:9, 6]
+
+    # Test everything below uppper triangular portion
+    # is zero
+    RLinearAlgebra.gentleman!(G)
+    for i in 1:n
+        for j in (i+1):m
+            @test G.B[j,i] == 0
+        end
+
+    end
+
+    # Test ldiv!
+    y = zeros(5)
+    ldiv!(y, G, b)
+    @test norm(y - x) < 1e2 * eps()
+
+    # Test Reset gent makes everything zero 
+    RLinearAlgebra.reset_gent!(G)
+    for i in 1:n  
+        for j in 1:m
+            @test G.B[j,i] == 0
+        end
+
+        @test G.B[i] == 0
+    end
+
+end
+
+end # End module
