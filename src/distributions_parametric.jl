bbfb974105bf811cae52e2f234c5918af495397e
Christian Varner <varner.christian.a@gmail.com>
2024-08-02 09:42:19 -0500

    documentation

--- src/distributions_parametric.jl
+++ src/distributions_parametric.jl
@@ -1,6 +1,6 @@
 # Date: 08/01/2024
 # Author: Christian Varner
-# Purpose: Parametric types for distributions
+# Purpose: Parametric types for distributions.
 
 ##############################################################################
 ## This file is part of RLinearAlgebra.jl
@@ -9,7 +9,8 @@
 ## over rows and columns of a matrix. 
 ##
 ## Contents
-## - Abstract Types
+## - Abstract (parametric) Types
+## - `initialize` function documentation and interface
 ## - `getDistribution` function documentation and interface.
 ## - Include statements
 ##
@@ -28,35 +29,75 @@ abstract type Left <: SketchDirection end # comes from rows
 abstract type Right <: SketchDirection end # comes from columns
 
 """
+    Distribution{T <: SketchDirection}
+
+Parametric type that represents a distribution over the rows when
+`T <: Left`, and over the columns when `T <: Right`. Corresponds
+to left and right sketching of a matrix.
 """
-abstract type ParametricDistribution{T<:SketchDirection} end
+abstract type ParametricDistribution{T <: SketchDirection} end
+
+"""
+    Base.eltype(dist::Distribution{T}) 
+
+Function to return the element-type `T` of Distribution{T}.
+"""
 function Base.eltype(dist::ParametricDistribution{T}) where {T}
     return T
 end
 
 """
+    initialize(
+        dist::Distribution{<:SketchDirection}, 
+        A::AbstractArray
+    ) 
+
+A method for specifying a distribution over the rows or columns of the matrix
+`A`, depending on the element-type parameter `T`. Calls the common interface method
+`getDistribution`.
+
+The value returned by `initialize` will depend on the specific `Distribution` that is used
+and the parameter `T`.
+- For `T <: Left`, a `Weights` vector of size `size(A)[1]` will be returned that has sum approximately 1 using the method specified by `Distribution`.
+- For `T <: Right`, a `Weights` vector of size `size(A)[2]` will be returned that has sum approximately 1 using the method specified by `Distribution`.
 """
-function initialize(dist::ParametricDistribution{T}, A::AbstractArray) where {T}
+function initialize(
+    dist::ParametricDistribution{<:SketchDirection}, 
+    A::AbstractArray
+) where {T}
+
     if T == Left
         B = A
     elseif T == Right
         B = A'
+    elseif T == SketchDirection
+        B = A
+        @warn("SketchDirection is ambigious, returning distribution over rows of A.")
     end
     return getDistributionParametric(dist, B)
 end
 
 """
+    getDistribution(
+        dist::Distribution{<:SketchDirection},
+        B::AbstractArray
+    )
+
+A common interface responsible for creating a distribution over the rows of the matrix B.
+
+The output of this function will depend on `dist::Distribution`.
 """
 function getDistributionParametric(
     dist::ParametricDistribution{<:SketchDirection},
-    A::AbstractArray
-) where {T}
+    B::AbstractArray
+)
+
     return nothing
 end
 
 
 ###############################
-# Distributions
+# Distribution Imports
 ###############################
 include("parametric_distributions/parametric_frobenius_norm.jl")
 include("parametric_distributions/parametric_leverage_scores.jl")
