32c4369751ab19cb31584af2a092d35a07be8754
Vivak <laptop@wisc.edu>
2024-08-28 11:58:40 -0400

    Edits to docstring for SEDistInfo

--- src/linear_solver_logs/solve_log_ma.jl
+++ src/linear_solver_logs/solve_log_ma.jl
@@ -37,18 +37,23 @@ end
 """ 
     SEDistInfo
 
-A mutable structure that stores information about the sub-Exponential family.
+A mutable structure that stores information about a distribution (i.e., sampling method)
+in the sub-Exponential family.
 
 # Fields
 - `sampler::Union{DataType, Nothing}`, the type of sampling method.
 - `dimension::Int64`, the dimension that of the space that is being sampled.
 - `block_dimension::Int64`, the dimension of the sample.
-- `sigma2::Union{Float64, Nothing}`, the variance parameter in the sub-Exponential family, 
-   if not specified by the user it is provided based on the sampling method.
-- `omega::Union{Float64, Nothing}`, the exponential distrbution parameter, if not specified by the user, 
-   it is provided based on the sampling method.
-- `eta::Float64`, a parameter for adjusting the conservativeness of the distribution, higher value means a less conservative estimate. By default, this is set to `1`.
-- `scaling::Float64`, a scaling parameter for the norm-squared of the sketched residual to ensure its expectation is the norm-squared of the residual.
+- `sigma2::Union{Float64, Nothing}`, the variance parameter in the sub-Exponential family. 
+    If not specified by the user, a value is selected from a table based on the `sampler`. 
+    If the `sampler` is not in the table, then ?.
+- `omega::Union{Float64, Nothing}`, the exponential distrbution parameter. If not specified 
+    by the user, a value is selected from a table based on the `sampler`.
+    If the `sampler` is not in the table, then ?.
+- `eta::Float64`, a parameter for adjusting the conservativeness of the distribution, higher 
+    value means a less conservative estimate. A recommended value is `1`.
+- `scaling::Float64`, a scaling parameter for the norm-squared of the sketched residual to 
+    ensure its expectation is the norm-squared of the residual.
 
 For more information see:
 - Pritchard, Nathaniel, and Vivak Patel. "Solving, tracking and stopping streaming linear 
