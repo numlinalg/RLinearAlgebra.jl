4d210d9f82d663c9bd8c4de2ac312502ef03f4a9
Christian Varner <varner.christian.a@gmail.com>
2024-09-23 15:26:28 -0500

    removing some extra allocations from the arnoldi solver

--- src/linear_solver_routines/arnoldi_solver.jl
+++ src/linear_solver_routines/arnoldi_solver.jl
@@ -3,11 +3,12 @@
 # Purpose: Implement deterministic solver.
 
 """
-    arnoldi!(x, A, b, k)
+    arnoldi_solver!(x, A, b, k)
 
 Finds an approximate solution to the linear system `Ax = b` by doing an Arnoldi Iteration
-to build an orthonormal basis for the Krylov Subspace of size `k`. The method was detailed
-in
+to build an orthonormal basis for the Krylov Subspace of size `k`. 
+    
+The method was detailed in
 
 Timsit, Grigori, and Balabanov. "Randomized Orthogonal Projection Methods for Krylov Subspace Solvers."
 https://arxiv.org/abs/2302.07466.
@@ -25,7 +26,7 @@ function arnoldi_solver!(
     k::Int
 )
 
-    # helper function to form and solve the linear system
+    # helper function to form and solve the linear system after main loop
     function solve_linear_system!(
         x::AbstractVector, 
         beta::Float64, 
@@ -42,24 +43,29 @@ function arnoldi_solver!(
     # initialization of storage and initial residual
     r0 = b - A * x 
     beta = norm(r0)
+    d = zeros(size(A)[1])
     V = zeros(size(A)[1], k+1)
     H = zeros(k+1, k+1)
 
-    V[:, 1] = r0 / beta 
+    V[:, 1] .= r0 ./ beta 
     for j in 1:k
         # next vector to add to basis
-        z = A * V[:, j]
+        z = view(V, :, j+1)
+        mul!(z, A, view(V, :, j))
 
         # orthogonalization constants
-        H[1:j, j] = V[:, 1:j]' * z
+        buffer = view(H, 1:j, j)
+        mul!(buffer, view(V, :, 1:j)', z)
 
         # orthogonalize and update basis
-        z = z - V[:, 1:j] * H[1:j, j]
+        mul!(d, view(V, :, 1:j), view(H, 1:j, j))
+        z .-= d
+        
         H[j+1,j] = norm(z)
-        V[:,j+1] = z / H[j+1,j]
+        z ./= H[j+1,j]
     end
 
     # solve linear system
-    solve_linear_system!(x, beta, H, V, k)
+    #solve_linear_system!(x, beta, H, V, k)
     return H, V
 end
