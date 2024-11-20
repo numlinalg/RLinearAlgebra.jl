3ca98342bc5fd6baff495588035f4dada9b743cf
Tunan Wang <Tunan.wang@wisc.edu>
2024-11-20 07:20:51 -0600

    Change the PR

--- src/linear_samplers/block_col_sparse_sign.jl
+++ src/linear_samplers/block_col_sparse_sign.jl
@@ -5,34 +5,40 @@
     LinSysBlkColSparseSign <: LinSysBlkColSampler
 
 A mutable structure with fields to handle sparse sign column sketching where a sparse sign matrix
-is multiplied by the matrix `A'` from the left. Methods are implemented as mentioned in section 9.2 
-of "Martinsson P. G., Tropp J. A. Randomized numerical linear algebra: Foundations and algorithms." 
-Acta Numerica, 2020, 29: 403-572.
+is multiplied by the matrix `A'` from the left. More details of the methods are mentioned in the 
+section 9.2 of 
+
+Martinsson P-G, Tropp JA. "Randomized numerical linear algebra: Foundations and algorithms." 
+Acta Numerica. 2020;29:403-572. doi:10.1017/S0962492920000021.
 
 # Fields
-- `block_size::Int64`: Represents the number of rows in the sketch matrix.
-- `sparsity::Float64`: Represents the sparsity of the sketch matrix. Suppose the sketch matrix 
+- `block_size::Int64`, represents the number of rows in the sketch matrix.
+- `sparsity::Float64`, represents the sparsity of the sketch matrix. Suppose the sketch matrix 
     has dimensions of `d` rows and `n` columns; the sparsity, as described in the reference book, 
     can be chosen from `{2, 3, ..., d}`, representing the number of elements we want in each column.
-- `sketch_matrix::Union{AbstractMatrix, Nothing}`: Buffer for storing the Gaussian sketching matrix.
-- `numsigns::Int64`: Buffer for storing how many signs we need to have for each column of the 
+- `sketch_matrix::Union{AbstractMatrix, Nothing}`, buffer for storing the sparse sign sketching matrix.
+- `numsigns::Int64`, buffer for storing how many signs we need to have for each column of the 
     sketch matrix, calculated by `max(floor(sparsity * size(A, 1)), 2)`.
-- `scaling::Float64`: The standard deviation of the sketch, set to `sqrt(n / numsigns)`.
+- `scaling::Float64`, the standard deviation of the sketch, set to `sqrt(n / numsigns)`.
 
 # Constructors
 - `LinSysBlkColSparseSign()` defaults to setting `block_size` to 8 and `sparsity` to `min(d, 8)`.
 """
 mutable struct LinSysBlkColSparseSign <: LinSysBlkColSampler
     block_size::Int64
+    function LinSysBlkRowSparseSign(block_size::Int64)
+        @assert block_size > 0 "`block_size` must be positive."
+        return new(block_size)
+    end
     sparsity::Float64
-    sketch_matrix::Union{AbstractMatrix, Nothing}
+    sketch_matrix::Union{Matrix{Int64}, Nothing}
     numsigns::Int64
     scaling::Float64
 end
 
-LinSysBlkColSparseSign(block_size, sparsity) = LinSysBlkColSparseSign(block_size, sparsity,
+LinSysBlkColSparseSign(;block_size, sparsity) = LinSysBlkColSparseSign(block_size, sparsity,
     nothing, 0, 0.0)
-LinSysBlkColSparseSign(block_size) = LinSysBlkColSparseSign(block_size, -12345.0, nothing, 0, 
+LinSysBlkColSparseSign(;block_size) = LinSysBlkColSparseSign(block_size, -12345.0, nothing, 0, 
     0.0)
 LinSysBlkColSparseSign(;sparsity) = LinSysBlkColSparseSign(8, sparsity, nothing, 0, 0.0)
 LinSysBlkColSparseSign() = LinSysBlkColSparseSign(8, -12345.0, nothing, 0, 0.0)
@@ -45,42 +51,41 @@ function sample(
     x::AbstractVector,
     iter::Int64
 )
-    # sketch matrix has dimension d (a pre-identified number of rows) by n (matrix A's 
-    # number of Columns)
-    d, n = type.block_size, size(A,2)
+    # Sketch matrix has dimension type.block_size (a pre-identified number of rows) by 
+    # size(A,2) (matrix A's number of Columns)
 
     if iter == 1
-        @assert type.block_size > 0 "`block_size` must be positve."
-        if type.block_size > n
+        # @assert type.block_size > 0 "`block_size` must be positve."
+        if type.block_size > size(A,2)
             @warn "`block_size` should be less than or equal to column dimension"
         end
 
-        # In default, we should sample min{d, 8} signs for each column.
-        # Otherwise, we take an integer from 2 to d with sparsity parameter.
+        # In default, we should sample min{type.block_size, 8} signs for each column.
+        # Otherwise, we take an integer from 2 to type.block_size with sparsity parameter.
         if type.sparsity == -12345.0
-            type.numsigns = min(d, 8)
+            type.numsigns = min(type.block_size, 8)
         elseif type.sparsity <= 0.0 || type.sparsity >= 1.0
             DomainError(sparsity, "Must be strictly between 0.0 and 1.0") |>
                 throw
         else
-            type.numsigns = max(floor(Int64, type.sparsity * n), 2)
+            type.numsigns = max(floor(Int64, type.sparsity * size(A,2)), 2)
         end
 
         # Scaling value for saprse sign matrix
-        type.scaling = sqrt(n / type.numsigns)
+        type.scaling = sqrt(size(A,2) / type.numsigns)
 
         # Initialize the sparse sign matrix and assign values to iter
-        type.sketch_matrix = zeros(Int64, d, n)
+        type.sketch_matrix = zeros(Int64, type.block_size, size(A,2))
 
     end
 
     # Create a random matrix with 1 and -1
-    type.sketch_matrix = ifelse.(rand(d, n) .> 0.5, 1, -1)  
+    type.sketch_matrix = ifelse.(rand(type.block_size, n) .> 0.5, 1, -1)  
 
-    # Each column we want to have d - type.numsigns non-zero terms
-    if d != type.numsigns
-        for i in 1:n
-            row_perm = randperm(d)[1:(d - type.numsigns)]
+    # Each column we want to have type.block_size - type.numsigns non-zero terms
+    if type.block_size != type.numsigns
+        for i in 1:size(A,2)
+            row_perm = randperm(type.block_size)[1:(type.block_size - type.numsigns)]
             type.sketch_matrix[row_perm, i] = 0
         end
     end
--- src/linear_samplers/block_row_sparse_sign.jl
+++ src/linear_samplers/block_row_sparse_sign.jl
@@ -4,19 +4,22 @@
     LinSysBlkRowSparseSign <: LinSysBlkRowSampler
 
 A mutable structure with fields to handle sparse sign row sketching where a sparse sign matrix
-is multiplied by the matrix `A` from the left. Methods are implemented as mentioned in section 9.2 
-of "Martinsson P. G., Tropp J. A. Randomized numerical linear algebra: Foundations and algorithms." 
-Acta Numerica, 2020, 29: 403-572.
+is multiplied by the matrix `A` from the left. More details of the methods are mentioned in the 
+section 9.2 of 
+
+Martinsson P-G, Tropp JA. "Randomized numerical linear algebra: Foundations and algorithms." 
+Acta Numerica. 2020;29:403-572. doi:10.1017/S0962492920000021.
+
 
 # Fields
-- `block_size::Int64`: Represents the number of rows in the sketch matrix.
-- `sparsity::Float64`: Represents the sparsity of the sketch matrix. Suppose the sketch matrix 
-    has dimensions of `d` rows and `n` columns; the sparsity, as described in the reference book, 
-    can be chosen from `{2, 3, ..., d}`, representing the number of elements we want in each column.
-- `sketch_matrix::Union{AbstractMatrix, Nothing}`: Buffer for storing the Gaussian sketching matrix.
-- `numsigns::Int64`: Storing how many signs we need to have for each column of the 
+- `block_size::Int64`, represents the number of rows in the sketch matrix.
+- `sparsity::Float64`, represents the sparsity of the sketch matrix. Suppose the sketch matrix 
+    has dimensions of `block_size` rows and `n` columns; the sparsity, as described in the reference book, 
+    can be chosen from `{2, 3, ..., block_size}`, representing the number of elements we want in each column.
+- `sketch_matrix::Union{AbstractMatrix, Nothing}`, buffer for storing the sparse sign sketching matrix.
+- `numsigns::Int64`, storing how many signs we need to have for each column of the 
     sketch matrix, calculated only in the first iteration by `max(floor(sparsity * size(A, 1)), 2)`.
-- `scaling::Float64`: The standard deviation of the sketch, set to `sqrt(n / numsigns)`, calculated 
+- `scaling::Float64`, the standard deviation of the sketch, set to `sqrt(n / numsigns)`, calculated 
     only in the first iteration.
 
 # Constructors
@@ -24,15 +27,20 @@ Acta Numerica, 2020, 29: 403-572.
 """
 mutable struct LinSysBlkRowSparseSign <: LinSysBlkRowSampler
     block_size::Int64
+    function LinSysBlkRowSparseSign(block_size::Int64)
+        @assert block_size > 0 "`block_size` must be positive."
+        return new(block_size)
+    end
     sparsity::Float64
-    sketch_matrix::Union{AbstractMatrix, Nothing}
+    
+    sketch_matrix::Union{Matrix{Int64}, Nothing}
     numsigns::Int64
     scaling::Float64
 end
 
-LinSysBlkRowSparseSign(block_size, sparsity) = LinSysBlkRowSparseSign(block_size, sparsity,
+LinSysBlkRowSparseSign(;block_size, sparsity) = LinSysBlkRowSparseSign(block_size, sparsity,
     nothing, 0, 0.0)
-LinSysBlkRowSparseSign(block_size) = LinSysBlkRowSparseSign(block_size, -12345.0, nothing, 0, 
+LinSysBlkRowSparseSign(;block_size) = LinSysBlkRowSparseSign(block_size, -12345.0, nothing, 0, 
     0.0)
 LinSysBlkRowSparseSign(;sparsity) = LinSysBlkRowSparseSign(8, sparsity, nothing, 0, 0.0)
 LinSysBlkRowSparseSign() = LinSysBlkRowSparseSign(8, -12345.0, nothing, 0, 0.0)
@@ -45,41 +53,40 @@ function sample(
     x::AbstractVector,
     iter::Int64
 )
-    # sketch matrix has dimension d (a pre-identified number of rows) by n (matrix A's 
-    # number of rows)
-    d, n = type.block_size, size(A,1)
+    # Sketch matrix has dimension type.block_size (a pre-identified number of rows) by 
+    #size(A,1) (matrix A's number of rows)
 
     if iter == 1
-        @assert type.block_size > 0 "`block_size` must be positve."
-        if type.block_size > n
+        # @assert type.block_size > 0 "`block_size` must be positve."
+        if type.block_size > size(A,1)
             @warn "`block_size` should be less than or equal to row dimension"
         end
 
-        # In default, we should sample min{d, 8} signs for each column.
+        # In default, we should sample min{type.block_size, 8} signs for each column.
         # Otherwise, we take an integer from 2 to d with sparsity parameter.
         if type.sparsity == -12345.0
-            type.numsigns = min(d, 8)
+            type.numsigns = min(type.block_size, 8)
         elseif type.sparsity <= 0.0 || type.sparsity >= 1.0
             DomainError(sparsity, "Must be strictly between 0.0 and 1.0") |>
                 throw
         else
-            type.numsigns = max(floor(Int64, type.sparsity * n), 2)
+            type.numsigns = max(floor(Int64, type.sparsity * size(A,1)), 2)
         end
 
         # Scaling value for saprse sign matrix
-        type.scaling = sqrt(n / type.numsigns)
+        type.scaling = sqrt(size(A,1) / type.numsigns)
 
         # Initialize the sparse sign matrix and assign values to iter
-        type.sketch_matrix = zeros(Int64, d, n)
+        type.sketch_matrix = zeros(Int64, type.block_size, size(A,1))
     end
 
     # Fill the sketch matrix with 1 and -1
-    type.sketch_matrix = ifelse.(rand(d, n) .> 0.5, 1, -1)  
+    type.sketch_matrix = ifelse.(rand(type.block_size, size(A,1)) .> 0.5, 1, -1)  
 
-    # Each column we want to have d - type.numsigns non-zero terms
-    if d != type.numsigns
-        for i in 1:n
-            row_perm = randperm(d)[1:(d - type.numsigns)]
+    # Each column we want to have type.block_size - type.numsigns non-zero terms
+    if type.block_size != type.numsigns
+        for i in 1:size(A,1)
+            row_perm = randperm(type.block_size)[1:(type.block_size - type.numsigns)]
             type.sketch_matrix[row_perm, i] = 0
         end
     end
