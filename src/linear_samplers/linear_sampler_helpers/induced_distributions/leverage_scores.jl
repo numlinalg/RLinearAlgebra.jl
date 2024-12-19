d3e1a8d07f0c2b32881a043145715c87a6eaf39e
Christian Varner <varner.christian.a@gmail.com>
2024-12-19 08:04:08 -0600

    rough draft of leverage_scores

---
+++ src/linear_samplers/linear_sampler_helpers/induced_distributions/leverage_scores.jl
@@ -0,0 +1,28 @@
+# Date: 12/19/2024
+# Author: Christian Varner
+# Purpose: Create functionality for distributions for rows and columns based
+# on the leverage scores.
+
+"""
+    TODO - documentation
+"""
+function leverage_score_distribution(
+    A::AbstractMatrix,
+    row_distribution::Bool
+)
+    # error checking
+    #@assert row_distribution ? (size(A)[1] >= size(A)[2]) : (size(A')[1] <= size(A')[2])
+    #"Need to take QR decomposition but dimensions of `A` are incorrect."
+
+    # get thin QR
+    sz = row_distribution ? size(A)[1] : size(A)[2]
+    distribution = zeros(sz)
+    Q1 = row_distribution ? collect(qr(A).Q) : collect(qr(A').Q)
+    
+    # compute distribution
+    for i in 1:sz
+        distribution[i] = row_distribution ? norm(view(Q1, i, :))^2 : norm(view(Q1, i, :))^2
+    end
+
+    return distribution ./ sum(distribution)
+end
