009c4a4985f5e8437d78b1b22078a047a2e740f3
“Tunan <tunan.wang@wisc.edu>
2024-12-23 23:12:39 +0800

    Doc issue

--- src/linear_samplers/block_col_sparse_sign.jl
+++ src/linear_samplers/block_col_sparse_sign.jl
@@ -57,16 +57,14 @@ function sample(
     # size(A,2) (matrix A's number of Columns)
     if iter == 1
         type.block_size > size(A, 2) && @warn "`block_size` should less than or \
-                                               equal to column dimension, $(size(A,2))"
+        equal to column dimension, $(size(A,2))"
 
         # In default, we should sample min{type.block_size, 8} signs for each column.
         # Otherwise, we take an integer from 2 to type.block_size.
         type.numsigns == nothing && (type.numsigns = min(type.block_size, 8))
         type.numsigns <= 0 && @assert type.numsigns > 0 "`numsigns` must be positive."
         type.numsigns > 0 && @assert (type.numsigns <= type.block_size) "`numsigns` must less \
-                                                                         than the block size \
-                                                                         of sketch matrix, \
-                                                                         $(type.block_size)."
+        than the block size of sketch matrix, $(type.block_size)."
 
 
         # Scaling value for saprse sign matrix
