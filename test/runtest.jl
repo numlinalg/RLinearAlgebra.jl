38c82e111b79c5c51fb43bbab3a84f9100586802
Nathaniel pritchard <npritchard@wisc.edu>
2025-01-27 17:06:56 +0000

    added runtest file to branch

---
+++ test/runtest.jl
@@ -0,0 +1,23 @@
+# This file is part of RLinearAlgebra.jl
+using Test
+
+# List all directories that have files to be tested 
+directs = joinpath.(@__DIR__,["./", "Solvers/", "Solvers/Loggers/", "Solvers/SubSolvers/", "Compressors/"])
+
+@testset verbose=true "RLinearAlbera.jl" begin
+    for direct in directs 
+        # Obtain all files in the directory
+        files_in_direct = readdir(direct)
+        # Only test files that end in .jl
+        files_to_test = files_in_direct[occursin.(r".jl$", files_in_direct)]
+        for file in files_to_test  
+            # Make sure you do not call the runtest file otherwise you have infinite recursion
+            if file != "runtest.jl"
+                include(direct * file)
+            end
+
+        end
+
+    end
+
+end
