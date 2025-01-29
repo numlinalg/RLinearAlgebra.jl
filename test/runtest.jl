2a1099a83eb0cb71fbbccaaef2e21f38c4fdf49e
Nathaniel pritchard <npritchard@wisc.edu>
2025-01-29 11:58:19 +0000

    updated runtest with atol and added field macros

--- test/runtest.jl
+++ test/runtest.jl
@@ -1,8 +1,19 @@
 # This file is part of RLinearAlgebra.jl
 using Test
 
+# Modify ≈ to have a global tolerance
+ATOL = 1e-10
+import Base.≈
+≈(a::Float64, b::Float64) = isapprox(a, b, atol = ATOL)
+# Adjust for tor the sum of multiple elements
+≈(a::AbstractArray, b::AbstractArray) = isapprox(a, b, atol = .5 * ATOL * prod(size(a)))
+
+# Include the test for recipes
+include("./test_helpers/field_test_macros.jl")
+
 # List all directories that have files to be tested 
-directs = joinpath.(@__DIR__,["./", "Solvers/", "Solvers/Loggers/", "Solvers/SubSolvers/", "Compressors/"])
+directs = joinpath.(@__DIR__,["./", "Approximators/", "Solvers/", "Solvers/Loggers/", 
+                              "Solvers/SubSolvers/", "Compressors/"])
 
 @testset verbose=true "RLinearAlbera.jl" begin
     for direct in directs 
