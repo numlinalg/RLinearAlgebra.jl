7c2f951cccf31c60b92087ed6f572010b140ccec
Nathaniel pritchard <npritchard@wisc.edu>
2025-02-03 19:01:16 +0000

    changed file name to runtests

---
+++ test/runtests.jl
@@ -0,0 +1,28 @@
+using Test
+using RLinearAlgebra
+
+
+# Include the test for recipes
+include("./test_helpers/field_test_macros.jl")
+
+# List all directories that have files to be tested 
+directs = joinpath.(@__DIR__,["./", "Approximators/", "Solvers/", "Solvers/Loggers/", 
+                              "Solvers/SubSolvers/", "Compressors/"])
+
+@testset verbose=true "RLinearAlbera.jl" begin
+    for direct in directs 
+        # Obtain all files in the directory
+        files_in_direct = readdir(direct)
+        # Only test files that end in .jl
+        files_to_test = files_in_direct[occursin.(r".jl$", files_in_direct)]
+        for file in files_to_test  
+            # Make sure you do not call the runtest file otherwise you have infinite recursion
+            if file != "runtests.jl"
+                include(direct * file)
+            end
+
+        end
+
+    end
+
+end
