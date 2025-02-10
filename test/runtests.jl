0454c8afeec1345915e8acb8bb29d24dd7c3d537
Nathaniel pritchard <npritchard@wisc.edu>
2025-02-10 17:18:57 +0000

    removed files

--- test/runtests.jl
+++
@@ -1,9 +0,0 @@
-# This file is part of RLinearAlgebra.jl
-
-using Test
-
-@testset verbose=true "RLinearAlbera.jl" begin
-    for file in readlines(joinpath(@__DIR__, "testgroups.txt"))
-        include(file * ".jl")
-    end
-end
