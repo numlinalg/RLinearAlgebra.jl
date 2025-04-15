0cadca95a8c842530c439bb6fc749a326812b2ef
Vivak Patel <vp314@users.noreply.github.com>
2025-04-15 10:40:25 -0500

    Added abstract mul error checks to this file (moved some from multiplication error tests)

--- test/Compressors/compressor_abstract_types.jl
+++ test/Compressors/compressor_abstract_types.jl
@@ -1,5 +1,6 @@
 module compressor_abstract_types
 using Test, RLinearAlgebra
+import LinearAlgebra: mul!
 using ..FieldTest
 using ..ApproxTol
 struct TestCompressor <: Compressor end
@@ -26,6 +27,38 @@ end
     @test_throws ArgumentError update_compressor!(TestCompressorRecipe(), A)
     @test_throws ArgumentError update_compressor!(TestCompressorRecipe(), A, b)
     @test_throws ArgumentError update_compressor!(TestCompressorRecipe(), x, A, b)
+end
+
+@testset "Compressor Recipe Multiplication Errors" begin 
+    x = randn(2)
+    y = randn(2)
+    A = randn(2, 2)
+    C = randn(2, 2) 
+
+    # Five argument muls 
+    @test_throws ArgumentError mul!(C, TestCompressorRecipe(), A, 1.0, 1.0)
+    @test_throws ArgumentError mul!(C, A, TestCompressorRecipe(), 1.0, 1.0)
+    @test_throws ArgumentError mul!(x, TestCompressorRecipe(), y, 1.0, 1.0)
+    @test_throws ArgumentError mul!(x, y, TestCompressorRecipe(), 1.0, 1.0)
+
+    # Three argument muls 
+    @test_throws ArgumentError mul!(C, TestCompressorRecipe(), A)
+    @test_throws ArgumentError mul!(C, A, TestCompressorRecipe())
+    @test_throws ArgumentError mul!(x, TestCompressorRecipe(), y)
+    #   No specific 3 arg mul defined for next case but it is correctly handled
+    @test_throws ArgumentError mul!(x, y, TestCompressorRecipe()) 
+
+    # Five argument muls for adjoint 
+    @test_throws ArgumentError mul!(C, TestCompressorRecipe()', A, 1.0, 1.0)
+    @test_throws ArgumentError mul!(C, A, TestCompressorRecipe()', 1.0, 1.0)
+    @test_throws MethodError mul!(y, TestCompressorRecipe()', x, 1.0, 1.0)
+
+    # Three arguments muls for adjoint 
+    @test_throws MethodError mul!(y, TestCompressorRecipe()', x)
+    @test_throws ArgumentError mul!(C, TestCompressorRecipe()', A)
+    @test_throws ArgumentError mul!(C, A, TestCompressorRecipe()')
+
+    
 end
 
 end
