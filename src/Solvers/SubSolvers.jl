368107a6e8ca49066ebbb343660d787c87ca4996
Nathaniel pritchard <npritchard@wisc.edu>
2025-05-05 13:26:55 -0500

    Chnaged throw statements

--- src/Solvers/SubSolvers.jl
+++ src/Solvers/SubSolvers.jl
@@ -45,13 +45,12 @@ $(sub_solver_method_description[:complete_sub_solver])
 - $(sub_solver_output_list[:sub_solver_recipe])
 """
 function complete_sub_solver(solver::SubSolver, A::AbstractArray)
-    throw(
+    return throw(
         ArgumentError(
             "No `complete_sub_solver!` method defined for a solver of type \
             $(typeof(solver)) and $(typeof(A))."
         )
     )
-    return nothing
 end
 
 """
@@ -67,23 +66,21 @@ $(sub_solver_method_description[:update_sub_solver])
 - Modifies the `SubSolverRecipe` in place and returns nothing.
 """
 function update_sub_solver!(solver::SubSolverRecipe, A::AbstractArray)
-    throw(
+    return  throw(
         ArgumentError(
             "No `update_sub_solver!` method defined for a solver of type $(typeof(solver))\
             and $(typeof(A))."
         )
     )
-    return nothing
 end
 
 function ldiv!(x::AbstractVector, solver::SubSolverRecipe, b::AbstractVector)
-    throw(
+    return throw(
         ArgumentError(
             "No `ldiv!` method defined for a solver of type $(typeof(solver)), \
             $(typeof(x)), and $(typeof(b))."
         )
     )
-    return nothing
 end
 
 ###########################################
