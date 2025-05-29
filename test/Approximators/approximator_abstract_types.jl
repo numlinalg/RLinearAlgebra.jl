58e49d0b1ce9a90c17278b263053d092427c9481
Vivak Patel <vp314@users.noreply.github.com>
2025-05-29 08:03:08 -0400

    Focused tests on abstract approximator and approximator recipe

--- test/Approximators/approximator_abstract_types.jl
+++ test/Approximators/approximator_abstract_types.jl
@@ -1,46 +1,43 @@
 module approximators_abstract_types
 using Test, RLinearAlgebra
-include("../test_helpers/field_test_macros.jl")
-include("../test_helpers/approx_tol.jl")
-struct TestCompressorRecipe <: CompressorRecipe end
+import RLinearAlgebra: complete_approximator, rapproximate!
+
+
+#############################
+# Initial Testing Parameters
+#############################
 struct TestApproximator <: Approximator end
 struct TestApproximatorRecipe <: ApproximatorRecipe
-    S::CompressorRecipe
+    code::Int64
 end
-TestApproximatorRecipe() = TestApproximatorRecipe(TestCompressorRecipe())
-struct TestApproximatorError <: ApproximatorError end
-struct TestApproximatorErrorRecipe <: ApproximatorErrorRecipe end
 
+########################################
+# Tests for Approximator Abstract Types
+########################################
 @testset "Approximator Abstract Types" begin
     @test isdefined(Main, :Approximator)
     @test isdefined(Main, :ApproximatorRecipe)
-    @test isdefined(Main, :ApproximatorError)
-    @test isdefined(Main, :ApproximatorErrorRecipe)
     @test isdefined(Main, :ApproximatorAdjoint)
 end
 
-@testset "Approximator Argument Errors" begin
-    A = rand(2, 2)
-
-    @test_throws ArgumentError rapproximate!(TestApproximatorRecipe(), A)
-    @test_throws ArgumentError rapproximate(TestApproximator(), A)
+@testset "Approximator Completion Errors" begin
+    A = rand(2,2)
+        
+    @test_throws ArgumentError complete_approximator(TestApproximator(), A)
 end
 
-# Test ApproximatorError argment error
-@testset "ApproximatorError Argument Errors" begin
-    A = rand(2, 2)
-    b = rand(2)
-    x = rand(2)
+#############################
+# Update Testing Parameters 
+#############################
+complete_approximator(approx::TestApproximator, A::AbstractMatrix) = 
+    TestApproximatorRecipe(1)
 
-    @test_throws ArgumentError complete_approximator_error(
-        TestApproximatorError(), TestApproximator(), A
-    )
-    @test_throws ArgumentError compute_approximator_error!(
-        TestApproximatorErrorRecipe(), TestApproximatorRecipe(), A
-    )
-    @test_throws MethodError compute_approximator_error(
-        TestApproximatorError(), TestApproximatorRecipe(), A
-    )
+@testset "Approximator Completion" begin
+    A =rand(2, 2)
+
+    approx = complete_approximator(TestApproximator(), A) 
+    @test approx isa TestApproximatorRecipe
+    @test approx.code == 1
 end
 
 end
