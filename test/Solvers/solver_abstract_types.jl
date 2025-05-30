bfd7233f3fee8bd8a93472c759be2c4ec4d1ca52
Vivak Patel <vp314@users.noreply.github.com>
2025-05-30 13:15:59 -0400

    Corrected tests for rsolve; added tests for correct execution of abstract solver interfaces

--- test/Solvers/solver_abstract_types.jl
+++ test/Solvers/solver_abstract_types.jl
@@ -1,11 +1,14 @@
 module solver_abstract_types
 using Test, RLinearAlgebra
+import RLinearAlgebra: complete_solver, rsolve!
 
 ###################################
 # Initial Testing Parameters   
 ###################################
 struct TestSolver <: Solver end
-struct TestSolverRecipe <: SolverRecipe end
+mutable struct TestSolverRecipe <: SolverRecipe 
+    code::Int64
+end
 
 @testset "Solver Abstract Types" begin
     @test isdefined(Main, :Solver)
@@ -18,12 +21,61 @@ end
     x = rand(2)
 
     @test_throws ArgumentError complete_solver(TestSolver(), x, A, b)
-    @test_throws ArgumentError rsolve!(TestSolverRecipe(), x, A, b)
+    @test_throws ArgumentError rsolve!(TestSolverRecipe(1), x, A, b)
+    @test_throws ArgumentError rsolve(TestSolver(), x, A, b)
 end
 
 ###################################
 # Updated Testing Parameters   
 ###################################
+complete_solver(
+    solver::TestSolver, 
+    x::AbstractVector, 
+    A::AbstractMatrix, 
+    b::AbstractVector
+) = TestSolverRecipe(1)
+rsolve!(
+    solver::TestSolverRecipe,
+    x::AbstractVector, 
+    A::AbstractMatrix, 
+    b::AbstractVector
+) = begin
+    solver.code = 2
+    return nothing
+end
 
+@testset "Solver Interfaces" begin
+
+    # Check complete_solver works as expected 
+    let A = rand(2, 2),
+        b = rand(2),
+        x = rand(2),
+        solver = TestSolver()
+
+        solver_recipe = complete_solver(solver, x, A, b)
+        @test solver_recipe isa TestSolverRecipe
+        @test solver_recipe.code == 1
+    end
+    
+    # Check rsolve! with solver recipe works as expected 
+    let A = rand(2, 2),
+        b = rand(2),
+        x = rand(2),
+        solver_recipe = TestSolverRecipe(1)
+
+        rsolve!(solver_recipe, x, A, b)
+        @test solver_recipe.code == 2
+    end
+
+    #Check rsolve with solver ingredients works as expected 
+    let A = rand(2, 2),
+        b = rand(2),
+        x = rand(2),
+        solver = TestSolver()
+
+        x, solver_recipe = rsolve(solver, x, A, b)
+        @test solver_recipe.code == 2
+    end
+end
 
 end
