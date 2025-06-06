a8d304cc552f10ce8911a1a2b8e72e6f3c03bcad
Nathaniel pritchard <npritchard@wisc.edu>
2025-06-06 11:24:04 +0100

    added the multiarray complete subsolver test

--- src/Solvers/SubSolvers.jl
+++ src/Solvers/SubSolvers.jl
@@ -20,6 +20,7 @@ sub_solver_arg_list = Dict{Symbol,String}(
     :sub_solver_recipe => "`solver::SubSolverRecipe`, a fully initialized realization for a
     linear sub-solver.",
     :A => "`A::AbstractArray`, a coefficient matrix or vector.",
+    :b => "`b::AbstractArray`, a constant matrix or vector.",
 )
 
 sub_solver_output_list = Dict{Symbol,String}(
@@ -53,9 +54,23 @@ function complete_sub_solver(solver::SubSolver, A::AbstractArray)
     )
 end
 
+"""
+    complete_sub_solver(solver::SubSolver, A::AbstractArray, b::AbstractArray)
+
+$(sub_solver_method_description[:complete_sub_solver])
+
+# Arguments
+- $(sub_solver_arg_list[:sub_solver])
+- $(sub_solver_arg_list[:A]) 
+- $(sub_solver_arg_list[:b]) 
+
+# Returns 
+- $(sub_solver_output_list[:sub_solver_recipe])
+"""
 function complete_sub_solver(solver::SubSolver, A::AbstractArray, b::AbstractArray)
     complete_sub_solver(solver, A)
 end
+
 """
     update_sub_solver!(solver::SubSolverRecipe, A::AbstractArray)
 
